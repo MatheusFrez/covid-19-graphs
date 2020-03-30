89bb2f67287a7c3cce76dd9dd0564a06c0b4b9d4
MatheusFrez <matheusfrez.nf@gmail.com>
2020-03-30 19:23:35 -0300

    First commit.

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+   <div>
+      <apexchart width="500" type="bar" :options="chartOptions" :series="series"></apexchart>
+   </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Dashboard extends Vue {
+   @Prop() private msg!: string;
+   private chartOptions: any = {
+      chart: {
+         id: 'vuechart-example'
+      },
+      xaxis: {
+         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+      }
+   }
+
+   private series: Array<any> = [{
+      name: 'series-1',
+      data: [30, 40, 35, 50, 49, 60, 70, 91]
+   }]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   router,
