da9a2a947d22cb98ac55d83dd6af138d99356c8f
MatheusFrez <matheusfrez.nf@gmail.com>
2020-04-08 20:23:09 -0300

    :construction: Made footer page.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <router-view/>
+    <footer class="footer mt-auto py-3 bg-primary text-white">
+      <i class="fas fa-coffee"></i>
+      <h1>Made with ☕️ and ❤️ by <a href="https://github.com/MatheusFrez" style="color: grey;" target="_blank">Matheus Frez</a></h1>
+    </footer>
   </div>
 </template>
 
