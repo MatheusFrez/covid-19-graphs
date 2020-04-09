6a27697d7754437fb037e268b0fc3b882eeb189c
MatheusFrez <matheusfrez.nf@gmail.com>
2020-04-08 21:49:50 -0300

    :construction: Created donate help on footer.

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,45 @@
   <div id="app">
     <router-view/>
     <footer class="footer mt-auto py-3 bg-primary text-white">
-      <i class="fas fa-coffee"></i>
+      <v-overlay v-if="showCoffeHelp">
+        <v-dialog
+          v-model="showCoffeHelp"
+          width="500"
+        >
+          <template v-slot:activator="{ on }">
+            <v-card>
+              <v-card-title
+                class="headline grey lighten-2"
+                primary-title
+              >
+                Donate <v-icon>mdi-cash</v-icon>
+                <v-divider style="margin-left: 5px; margin-right: 5px;"></v-divider>
+                <v-btn
+                  color="red lighten-2"
+                  dark
+                  v-on="on"
+                >
+                 Close
+                <v-icon color="red">
+                  mdi-close
+                </v-icon>
+              </v-btn>
+              </v-card-title>
+
+              <v-card-text>
+                Bitcoin address: 39W1F2otcudStPisUTpBPoh4ZKvpfoD81L
+              </v-card-text>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-overlay>
       <h1>Made with ☕️ and ❤️ by <a href="https://github.com/MatheusFrez" style="color: grey;" target="_blank">Matheus Frez</a></h1>
+      <h2>
+        Donates
+        <v-btn class="mx-2" fab small @click="showCoffeHelp = true"  color="orange darken-2">
+          <v-icon color="yellow">mdi-cash</v-icon>
+        </v-btn>
+      </h2>
     </footer>
   </div>
 </template>
@@ -30,3 +67,20 @@
   }
 }
 </style>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  data: () => ({
+  })
+})
+export default class AppVue extends Vue {
+  showCoffeHelp: boolean = false
+
+  async mounted() {
+  }
+
+}
+</script>
